15442442d2c74e85650cfe0c5f7d9a86c3b5d259
curry <curry@siiva.com>
2019-05-15 16:19:09 +0800

    add main

--- main.go
+++ main.go
@@ -10,12 +10,63 @@ import (
 	"github.com/LittleCurry/misc/globals"
 	"github.com/LittleCurry/marry/handle"
 	"github.com/LittleCurry/marry/config"
+	"github.com/LittleCurry/misc/driver"
+	"github.com/LittleCurry/misc/helpers"
 )
 
 var (
 	env        = flag.String("env", "dev", "Running Environment")
 	configFile = flag.String("config", "", "The path of configuration file")
 )
+
+/**
+ * 解析参数
+ */
+func parseFlag() {
+	flag.Parse()
+	if *configFile == "" {
+		fmt.Println("place provide the configuration file path!")
+		fmt.Println("Usage:\n --config configpath [--env prod]")
+	}
+}
+
+/**
+ * 载入配置文件
+ */
+func loadConfig() {
+
+	fmt.Println("config.AppConf:", config.AppConf)
+	fmt.Println("env:", *env)
+	fmt.Println("configFile:", *configFile)
+
+	helpers.LoadConfigAndSetupEnv(config.AppConf, *env, *configFile)
+}
+
+/**
+ * 初始化redis
+ */
+func initRedis() {
+	fmt.Println("configs.AppConf.RedisAddr:", config.AppConf.RedisAddr)
+	driver.RedisInit(config.AppConf.RedisAddr, 0)
+}
+
+/**
+ * 初始化redis
+ */
+func initMongo() {
+	fmt.Println("configs.AppConf.MongoAddr:", config.AppConf.MongoAddr)
+	driver.MongoInit(config.AppConf.MongoAddr)
+}
+
+/**
+ * 初始化orm引擎
+ */
+func initOrm() {
+	fmt.Println("configs.AppConf.DbDsn:", config.AppConf.DbDsn)
+	driver.OrmInit(config.AppConf.DbDsn)
+}
+
+
 
 func init() {
 	parseFlag()
